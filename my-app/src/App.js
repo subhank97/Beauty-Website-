69284e0f062c8f04fdfa9aed2c40c2c718784b64
Subhan Khan <subhank1997@gmail.com>
2022-06-21 11:57:22 -0400

    Added Homepage and NavBar component

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar';
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () =>{
+    return (
+        <div>
+        <nav>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Services</a></li>
+      </nav>
+        </div>
+    )
+}
+
+export default NavBar
