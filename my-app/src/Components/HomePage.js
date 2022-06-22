db75344844c8077ab8fc8a970582a7aa2e8d9203
Subhan Khan <subhank1997@gmail.com>
2022-06-21 20:27:03 -0400

    updated Homepage

--- my-app/src/Components/HomePage.js
+++ my-app/src/Components/HomePage.js
@@ -3,7 +3,19 @@ import '../App.css'
 
 function HomePage() {
     return (
-        <div>
+        <div className="homepage">
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <a href="Product">Product</a>
+                    </li>
+                    <li>
+                        <a href="About">About</a>
+                    </li>
+                </ul>
+                    <a className="cart" href="Cart">Cart</a>
+            </nav>
+            <h1>Beauty Store</h1>
         </div>
     )
 }
